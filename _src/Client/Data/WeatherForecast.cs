953caef72d2c54eb5a6c4f17fc6b02e773c38287
Deon <vanvuuren.de@gmail.com>
2023-02-23 08:44:49 +0000

    Added Gitignor

--- _src/Client/Data/WeatherForecast.cs
+++ _src/Client/Data/WeatherForecast.cs
@@ -12,10 +12,10 @@ public class WeatherForecast
     public ForecastScale? Summary => TemperatureC switch
     {
         (< 10) => ForecastScale.Freezing,
-        > 10 and < 18 => ForecastScale.Chilly,
-        > 18 and < 25 => ForecastScale.Mild,
-        > 25 and < 32 => ForecastScale.Warm,
-        < 32 => ForecastScale.Hell
+        > 10 and <= 18 => ForecastScale.Chilly,
+        > 18 and <= 25 => ForecastScale.Mild,
+        > 25 and <= 32 => ForecastScale.Warm,
+        > 32 => ForecastScale.Hell
     };
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
